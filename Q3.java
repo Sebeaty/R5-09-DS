cb3485451febc15574e4e38f68fdeabcb1e02cbc
Sebeaty <102726372+Sebeaty@users.noreply.github.com>
2024-11-20 14:05:16 +0100

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,12 @@
+public class Q3 
+{
+  public static void main(String[] args) 
+  {
+    for (int i =0; i < Integer.parseInt(args[0];i++)
+    {
+      System.out.println("*******");
+    }
+		
+	}
+
+}
