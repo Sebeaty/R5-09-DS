8ee6173586591c8581981ddc48f0f5b12a12d985
Sebeaty <102726372+Sebeaty@users.noreply.github.com>
2024-11-20 14:25:10 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,15 @@
+public class Q5 
+{
+  public static void main(String[] args) 
+  {
+    String s = "";
+    for (int i = Integer.parseInt(args[0]); i > 0;i--)
+    {
+      for (int n=i; n > 0;n--)
+      {
+        s+="*"
+      }
+      s+="\n";
+    }
+	}
+}
