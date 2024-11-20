8393ee6694ccaa519e0dc9ee0b4c9dc574c0fd98
Sebeaty <102726372+Sebeaty@users.noreply.github.com>
2024-11-20 14:14:39 +0100

    Create Q4.java

---
+++ Q4.java
@@ -0,0 +1,15 @@
+public class Q4 
+{
+  public static void main(String[] args) 
+  {
+    String s = "";
+    for (int i =0; i < Integer.parseInt(args[0];i++)
+    {
+      for (int n=0; n < i;n++)
+      {
+        s+="*"
+      }
+      s+="\n";
+    }
+	}
+}
